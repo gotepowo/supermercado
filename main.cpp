2bc8c3a26e38630f444123802295dfe4b084754a
gotepowo <tgotelips@gmail.com>
2025-06-05 19:46:41 -0400

    Funções ajustadas
    Criadas funções para fazer uma lista encadeada, inserir um novo nó no final da lista, exibir a lista e liberar a lista.

--- main.cpp
+++ main.cpp
@@ -7,8 +7,59 @@ struct produto{
 	char nome[100];
 	float preco;
 	int estoque;
+	struct produto *proximo;
 };
 
+produto* criarProduto(int codigo, const char* nome, float preco, int estoque){
+	produto *novoProduto = (produto*) malloc(sizeof(produto));
+
+	novoProduto -> codigo = codigo;
+	strcpy(novoProduto -> nome, nome);
+	novoProduto -> preco = preco;
+    novoProduto -> estoque = estoque;
+	novoProduto -> proximo = NULL;
+	return novoProduto;
+}
+
+produto* inserirNoFinal(produto *cabeca, produto *novoProduto){
+	if(cabeca == NULL){
+		return novoProduto;
+	}
+	produto *atual = cabeca;
+	while(atual -> proximo != NULL){
+		atual = atual -> proximo;
+	}
+	atual -> proximo = novoProduto;
+	return cabeca;
+}
+
+void exibirLista(produto *cabeca){
+    produto *atual = cabeca;
+    if (atual == NULL) {
+        printf("Lista de produtos vazia.\n");
+        return;
+    }
+    while (atual != NULL) {
+        printf("Codigo: %d\n", atual->codigo);
+        printf("Nome: %s\n", atual->nome);
+        printf("Preco: %.2f\n", atual->preco);
+        printf("Estoque: %d\n", atual->estoque);
+        if (atual->proximo != NULL) {
+            printf("\n");
+        }
+        atual = atual->proximo;
+    }
+}
+
+void liberarLista(produto *cabeca) {
+    produto *atual = cabeca;
+    produto *proximoNo;
+    while (atual != NULL) {
+        proximoNo = atual->proximo; 
+        free(atual);                
+        atual = proximoNo;          
+    }
+}
 
 int main(){
 	int qtdProdutos;
@@ -25,19 +76,4 @@ int main(){
 
 	produto produtos[qtdProdutos];
 
-	for(int i = 0; i < qtdProdutos; i++){
-		fscanf(arquivo, "%d", &produtos[i].codigo);
-		fscanf(arquivo, " %[^\n]", produtos[i].nome);
-		fscanf(arquivo, "%f", &produtos[i].preco);
-		fscanf(arquivo, "%d", &produtos[i].estoque);
-	}
-	for(int i = 0; i < qtdProdutos; i++){
-		printf("Codigo: %d\n", produtos[i].codigo);
-		printf("Nome: %s\n", produtos[i].nome);
-		printf("Preco: %.2f\n", produtos[i].preco);
-		printf("Estoque: %d\n", produtos[i].estoque);
-		if(i != qtdProdutos - 1){
-			printf("\n");
-		}
-	}
 }	
